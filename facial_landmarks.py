673e909ad599d9f3e926f56763c1bfbed2e9e4dd
LAPTOP-4IRHU3QS\sharath <sharathchandra13194@gmail.com>
2018-10-04 11:11:37 +1000

    Added Blink Detection and Drew countours for the eye. TO DO: Have to locate the iris and draw countours for the same.

--- facial_landmarks.py
+++ facial_landmarks.py
@@ -1,10 +1,30 @@
 # Facial landmarks
-
+from scipy.spatial import distance as dist
 from imutils import face_utils
 import numpy as np
 import imutils
 import dlib
 import cv2
+
+# Constants for controlling the Blink Frequency.
+ear_Treshold = 0.3
+blink_Consec_frames = 3
+counter = 0
+blinks = 0
+
+def eye_aspect_ratio(eye):
+	# compute the euclidean distances for the vertical eye landmarks (x, y)-coordinates
+	A = dist.euclidean(eye[1], eye[5])
+	B = dist.euclidean(eye[2], eye[4])
+
+	# compute the euclidean distance for horizontal eye landmark (x, y)-coordinates
+	C = dist.euclidean(eye[0], eye[3])
+
+	# compute the eye aspect ratio
+	ear = (A + B) / (2.0 * C)
+
+	return ear
+
 
 # initialize dlib's face detector (HOG-based) and then create
 # the facial landmark predictor
@@ -15,44 +35,73 @@ predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 camera = cv2.VideoCapture(0)
 
 while camera.isOpened():
-    ret, frame = camera.read()
-    
-    if ret == False:
-        break
-    
-    # load the input image, resize it, and convert it to grayscale
-    frame = imutils.resize(frame, width = 500)
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    
-    # detect faces in the grayscale image
-    rects = detector(gray, 1)
-    
-    # loop over the face detections
-    for (i, rect) in enumerate(rects):
-    	# determine the facial landmarks for the face region, then
-    	# convert the facial landmark (x, y)-coordinates to a NumPy
-    	# array
-    	shape = predictor(gray, rect)
-    	shape = face_utils.shape_to_np(shape)
-     
-    	# convert dlib's rectangle to a OpenCV-style bounding box
-    	# [i.e., (x, y, w, h)], then draw the face bounding box
-    	(x, y, w, h) = face_utils.rect_to_bb(rect)
-    	cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-     
-    	# show the face number
-    	cv2.putText(frame, "Face #{}".format(i + 1), (x - 10, y - 10),
-    		cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-     
-    	# loop over the (x, y)-coordinates for the facial landmarks
-    	# and draw them on the image
-    	for (x, y) in shape:
-    		cv2.circle(frame, (x, y), 1, (0, 0, 255), -1)
-            
-    cv2.imshow("WebCam", frame)
-    if cv2.waitKey(1) & 0xFF == 27:
-        break
-    
+	ret, frame = camera.read()
+
+	if ret == False:
+		break
+
+	# load the input image, resize it, and convert it to grayscale
+	frame = imutils.resize(frame, width=500)
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+	# detect faces in the grayscale image
+	rects = detector(gray, 1)
+
+	# loop over the face detections
+	for (i, rect) in enumerate(rects):
+		# determine the facial landmarks for the face region, then
+		# convert the facial landmark (x, y)-coordinates to a NumPy
+		# array
+		shape = predictor(gray, rect)
+		shape = face_utils.shape_to_np(shape)
+
+		# grab the indexes of the facial landmarks for the left and
+		# right eye, respectively
+		(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+		(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+
+		left_eye = shape[lStart:lEnd]
+		right_eye = shape[rStart:rEnd]
+		left_ear = eye_aspect_ratio(left_eye)
+		right_ear = eye_aspect_ratio(right_eye)
+
+		avg_ear = (left_ear + right_ear)/2.0
+
+		if(avg_ear < ear_Treshold):
+			counter+=1
+		else:
+			if counter>=blink_Consec_frames:
+				blinks+=1
+
+			counter = 0
+
+		# convert dlib's rectangle to a OpenCV-style bounding box
+		# [i.e., (x, y, w, h)], then draw the face bounding box
+		(x, y, w, h) = face_utils.rect_to_bb(rect)
+		cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+		cv2.drawContours(frame, [cv2.convexHull(left_eye)], -1, (255, 0, 0), 1)
+		cv2.drawContours(frame, [cv2.convexHull(right_eye)], -1, (255, 0, 0), 1)
+
+		# show the face number
+		cv2.putText(frame, "Face #{}".format(i + 1), (x - 10, y - 10),
+		            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+		cv2.putText(frame, "Blinks: {}".format(blinks), (10, 30),
+		            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+		cv2.putText(frame, "EAR: {}".format(avg_ear), (300, 30),
+		            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+		# loop over the (x, y)-coordinates for the facial landmarks
+		# and draw them on the image
+		for (x, y) in shape:
+			cv2.circle(frame, (x, y), 1, (0, 0, 255), -1)
+
+	cv2.imshow("WebCam", frame)
+	if cv2.waitKey(1) & 0xFF == 27:
+		break
+
 camera.release()
 cv2.destroyAllWindows()
 
